9f967b822146ec7ee18e18c18691c1a9a170baae
Karol Baraniecki <karol@baraniecki.eu>
2022-01-04 21:51:26 +0100

    checkout -> clone

--- from_google_sheets.js
+++ from_google_sheets.js
@@ -1,5 +1,5 @@
 const { execSync } = require('child_process');
 
 exports.handler = async (event, context) => {
-    execSync('git --version && git checkout https://github.com/informatyzacja-sspwr-projekty/Chatbot && cd Chatbot && git checkout development && git status', { stdio: [0, 1, 2] });
+    execSync('git --version && git clone https://github.com/informatyzacja-sspwr-projekty/Chatbot && cd Chatbot && git checkout development && git status', { stdio: [0, 1, 2] });
 }
