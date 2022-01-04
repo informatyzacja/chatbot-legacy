af0b25a27beaa613688b688261d893a9ec1cdacf
Karol Baraniecki <karol@baraniecki.eu>
2022-01-04 21:46:48 +0100

    WIP: from_google_sheets.js

---
+++ from_google_sheets.js
@@ -0,0 +1,5 @@
+const { execSync } = require('child_process');
+
+exports.handler = async (event, context) => {
+    execSync('git --version && git checkout https://github.com/informatyzacja-sspwr-projekty/Chatbot && cd Chatbot && git checkout development && git status', { stdio: [0, 1, 2] });
+}
