126bffe9c7fdc1bb6cd90d0dc0b58cfa4acc79ff
Michał Korczak <michal.k2001@gmail.com>
2023-05-06 02:09:45 +0200

    update spreadsheet variables

--- .github/fetch-chatbot-config.sh
+++ .github/fetch-chatbot-config.sh
@@ -7,8 +7,8 @@ set -o xtrace
 #
 # Variables
 #
-SPREADSHEET_ID='1z4sa4K54l5WGXcYj0Fi73Pb5euX5HBHxlvnykNBeAnE'
-SHEET_ID='1396758614'
+SPREADSHEET_ID='1nfjNC4MWmt6XItyb7r9eoAgB2nquvzNlwNJyIG6N27o'
+SHEET_ID='556175424'
 
 SHEET_URL="https://docs.google.com/spreadsheets/d/${SPREADSHEET_ID}/export"
 SHEET_URL+="?format=csv&id=${SPREADSHEET_ID}&gid=${SHEET_ID}"
